c116272e20d3d62daa67a4591ff141a1519aaac2
Madisen Vogel <madisen@Admins-Mac-mini.btm.private>
2022-12-19 12:33:33 -0700

    thought api router

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,25 @@
+const router = require('express').Router();
+
+const {
+    getAllThoughts,
+    getThoughtById,
+    newThought,
+    updateThought,
+    deleteThought,
+    addReaction,
+    deleteReaction
+} = require('../../controllers/thoughtController');
+
+// /api/thoughts
+router.route('/').get(getAllThoughts).post(newThought);
+
+// /api/thoughts/:thoughtId
+router.route('/:thoughtId').get(getThoughtById).put(updateThought).delete(deleteThought);
+
+// /api/thoughts/:thoughtId/reactions
+router.route('/:thoughtId/reactions').post(addReaction);
+
+// /api/thoughts/:thoughtId/reactions/:reactionId
+router.route('/:thoughtId/reactions/:reactionId').delete(deleteReaction);
+
+module.exports = router;
